50f0f29c870564f39edb41f82dd8dea92f4646fc
Katherine <122394999+beijingcorn54@users.noreply.github.com>
2026-06-09 17:34:03 -0400

    Updated based on added ingrained data

--- torqueAngle_Surfaces.m
+++ torqueAngle_Surfaces.m
@@ -1,18 +1,24 @@
 clear;
 close all;
 
+% Control (Dynamic) Variables
+export_to_pdf = false;
+pdf_file_name = "Torque and Angle Surfaces, All Speeds.pdf";
+speed_filter = false;
+by_incline = false;
+num_buckets = false;
+sub_highlight = [2];    % Cadence trials are never highlighted 
+                        % because many steps are of the same cadence. 
+                        % The average of these cadences are from 
+                        % multiple people, so one person cannot be highlighted
+sub_remove = [];
+
 % Static Variables
 force_threshold = 25;
 subjects = ["AB01", "AB02", "AB03", "AB04", "AB05", "AB06", "AB07", "AB08", "AB09", "AB10"];
 legLengths = [0.951, 0.921, 0.99, 0.96, 0.766, 0.918, 0.859, 0.991, 0.940, 0.815];
 speeds = ["s0x8", -0.8; "s1", -1; "s1x2", -1.2];
 inclines = ["i10", 10; "i5", 5; "i0", 0; "in5", -5; "in10", -10];
-
-% Control (Dynamic) Variables
-speed_filter = false;
-by_incline = false;
-export_to_pdf = true;
-pdf_file_name = "Torque and Angle Surfaces, All Speeds.pdf";
 
 % Load in Data
 directory = "/Users/kefuller/Fuller_Locolab/";
@@ -42,83 +48,100 @@ addpath(directory + 'computation_functions/');
 % Plot based off metric and joint
 % Ankle Torque
 if ~by_incline
-    make_surface(in10_A_t, speed_filter, directory, -10, "Ankle", "Torque", "Unkown Units");
-    make_surface(in5_A_t, speed_filter, directory, -5, "Ankle", "Torque", "Unkown Units");
-    make_surface(i0_A_t, speed_filter, directory, 0, "Ankle", "Torque", "Unkown Units");
-    make_surface(i5_A_t, speed_filter, directory, 5, "Ankle", "Torque", "Unkown Units");
-    make_surface(i10_A_t, speed_filter, directory, 10, "Ankle", "Torque", "Unkown Units");
+    make_surface(in10_A_t, speed_filter, directory, -10, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(in5_A_t, speed_filter, directory, -5, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i0_A_t, speed_filter, directory, 0, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i5_A_t, speed_filter, directory, 5, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i10_A_t, speed_filter, directory, 10, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
 end
 
 % Knee Torque
 if ~by_incline
-    make_surface(in10_K_t, speed_filter, directory, -10, "Knee", "Torque", "Unkown Units");
-    make_surface(in5_K_t, speed_filter, directory, -5, "Knee", "Torque", "Unkown Units");
-    make_surface(i0_K_t, speed_filter, directory, 0, "Knee", "Torque", "Unkown Units");
-    make_surface(i5_K_t, speed_filter, directory, 5, "Knee", "Torque", "Unkown Units");
-    make_surface(i10_K_t, speed_filter, directory, 10, "Knee", "Torque", "Unkown Units");
+    make_surface(in10_K_t, speed_filter, directory, -10, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(in5_K_t, speed_filter, directory, -5, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i0_K_t, speed_filter, directory, 0, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i5_K_t, speed_filter, directory, 5, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i10_K_t, speed_filter, directory, 10, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
 end
 
 % Ankle Angle
 if ~by_incline
-    make_surface(in10_A_a, speed_filter, directory, -10, "Ankle", "Angle", "Unkown Units");
-    make_surface(in5_A_a, speed_filter, directory, -5, "Ankle", "Angle", "Unkown Units");
-    make_surface(i0_A_a, speed_filter, directory, 0, "Ankle", "Angle", "Unkown Units");
-    make_surface(i5_A_a, speed_filter, directory, 5, "Ankle", "Angle", "Unkown Units");
-    make_surface(i10_A_a, speed_filter, directory, 10, "Ankle", "Angle", "Unkown Units");
+    make_surface(in10_A_a, speed_filter, directory, -10, "Ankle", "Angle", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(in5_A_a, speed_filter, directory, -5, "Ankle", "Angle", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i0_A_a, speed_filter, directory, 0, "Ankle", "Angle", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i5_A_a, speed_filter, directory, 5, "Ankle", "Angle", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i10_A_a, speed_filter, directory, 10, "Ankle", "Angle", "Unkown Units", num_buckets, sub_highlight, sub_remove);
 end
 
 % Knee Angle
 if ~by_incline
-    make_surface(in10_K_a, speed_filter, directory, -10, "Knee", "Angle", "Degrees");
-    make_surface(in5_K_a, speed_filter, directory, -5, "Knee", "Angle", "Degrees");
-    make_surface(i0_K_a, speed_filter, directory, 0, "Knee", "Angle", "Degrees");
-    make_surface(i5_K_a, speed_filter, directory, 5, "Knee", "Angle", "Degrees");
-    make_surface(i10_K_a, speed_filter, directory, 10, "Knee", "Angle", "Degrees");
+    make_surface(in10_K_a, speed_filter, directory, -10, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+    make_surface(in5_K_a, speed_filter, directory, -5, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+    make_surface(i0_K_a, speed_filter, directory, 0, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+    make_surface(i5_K_a, speed_filter, directory, 5, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+    make_surface(i10_K_a, speed_filter, directory, 10, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
 end
 
 % Plot based off incline
 % Incline -10
 if by_incline
-    make_surface(in10_A_t, speed_filter, directory, -10, "Ankle", "Torque", "Unkown Units");
-    make_surface(in10_A_a, speed_filter, directory, -10, "Ankle", "Angle", "Degrees");
-    make_surface(in10_K_t, speed_filter, directory, -10, "Knee", "Torque", "Unkown Units");
-    make_surface(in10_K_a, speed_filter, directory, -10, "Knee", "Angle", "Degrees");
+    make_surface(in10_A_t, speed_filter, directory, -10, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(in10_A_a, speed_filter, directory, -10, "Ankle", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+    make_surface(in10_K_t, speed_filter, directory, -10, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(in10_K_a, speed_filter, directory, -10, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
 end
 
 % Incline -5
 if by_incline
-    make_surface(in5_A_t, speed_filter, directory, -5, "Ankle", "Torque", "Unkown Units");
-    make_surface(in5_A_a, speed_filter, directory, -5, "Ankle", "Angle", "Degrees");
-    make_surface(in5_K_t, speed_filter, directory, -5, "Knee", "Torque", "Unkown Units");
-    make_surface(in5_K_a, speed_filter, directory, -5, "Knee", "Angle", "Degrees");
+    make_surface(in5_A_t, speed_filter, directory, -5, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(in5_A_a, speed_filter, directory, -5, "Ankle", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+    make_surface(in5_K_t, speed_filter, directory, -5, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(in5_K_a, speed_filter, directory, -5, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
 end
 
 % Incline 0
 if by_incline
-    make_surface(i0_A_t, speed_filter, directory, 0, "Ankle", "Torque", "Unkown Units");
-    make_surface(i0_A_a, speed_filter, directory, 0, "Ankle", "Angle", "Degrees");
-    make_surface(i0_K_t, speed_filter, directory, 0, "Knee", "Torque", "Unkown Units");
-    make_surface(i0_K_a, speed_filter, directory, 0, "Knee", "Angle", "Degrees");
+    make_surface(i0_A_t, speed_filter, directory, 0, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i0_A_a, speed_filter, directory, 0, "Ankle", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+    make_surface(i0_K_t, speed_filter, directory, 0, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i0_K_a, speed_filter, directory, 0, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
 end
 
 % Incline 5
 if by_incline
-    make_surface(i5_A_t, speed_filter, directory, 5, "Ankle", "Torque", "Unkown Units");
-    make_surface(i5_A_a, speed_filter, directory, 5, "Ankle", "Angle", "Degrees");
-    make_surface(i5_K_t, speed_filter, directory, 5, "Knee", "Torque", "Unkown Units");
-    make_surface(i5_K_a, speed_filter, directory, 5, "Knee", "Angle", "Degrees");
+    make_surface(i5_A_t, speed_filter, directory, 5, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i5_A_a, speed_filter, directory, 5, "Ankle", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+    make_surface(i5_K_t, speed_filter, directory, 5, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i5_K_a, speed_filter, directory, 5, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
 end
 
 % Incline 10
 if by_incline
-    make_surface(i10_A_t, speed_filter, directory, 10, "Ankle", "Torque", "Unkown Units");
-    make_surface(i10_A_a, speed_filter, directory, 10, "Ankle", "Angle", "Degrees");
-    make_surface(i10_K_t, speed_filter, directory, 10, "Knee", "Torque", "Unkown Units");
-    make_surface(i10_K_a, speed_filter, directory, 10, "Knee", "Angle", "Degrees");
+    make_surface(i10_A_t, speed_filter, directory, 10, "Ankle", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i10_A_a, speed_filter, directory, 10, "Ankle", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+    make_surface(i10_K_t, speed_filter, directory, 10, "Knee", "Torque", "Unkown Units", num_buckets, sub_highlight, sub_remove);
+    make_surface(i10_K_a, speed_filter, directory, 10, "Knee", "Angle", "Degrees", num_buckets, sub_highlight, sub_remove);
+end
+
+%% Export to a PDF
+if export_to_pdf
+    figs = findall(groot, 'Type', 'figure');
+
+    if isfile(pdf_file_name)
+        delete(pdf_file_name)
+    end
+    
+    for i = 1 : length(figs)
+        if i == 1 % First page creates the PDF
+            exportgraphics(figs(i), pdf_file_name,'ContentType', 'vector');
+        else
+            exportgraphics(figs(i), pdf_file_name, 'ContentType', 'vector', 'Append', true);
+        end
+    end
 end
 
 %% Helper Functions
-function make_surface(joint_data, speed_filter, directory, incline, joint_type, metric, units)
+function make_surface(joint_data, speed_filter, directory, incline, joint_type, metric, units, num_buckets, sub_highlight, sub_remove)
 % 1. Sorts data into vectors by cadence and normalized stride length
     % uses sort_a_vector function
     % eliminates zero/invalid entries
@@ -146,12 +169,16 @@ else
     filtered_joint_data = joint_data;
 end
 
+addpath(directory + 'computation_functions/');
+cadence = get_surface_matrix(filtered_joint_data, 1, num_buckets, sub_highlight, sub_remove);
+addpath(directory + 'computation_functions/');
+norm_SL = get_surface_matrix(filtered_joint_data, 2, num_buckets, sub_highlight, sub_remove);
+addpath(directory + 'computation_functions/');
+calculated_speed = get_surface_matrix(filtered_joint_data, 3, num_buckets, sub_highlight, sub_remove);
 
 % 3. Plot the heat maps
     % --- Plot 1: Cadence ---
     figure;
-    addpath(directory + 'computation_functions/');
-    cadence = get_surface_matrix(filtered_joint_data, 1);
     surf(cadence{1}, cadence{2}, cadence{3}, cadence{4});
 
     xlabel("Cadence (steps per minute)");
@@ -165,8 +192,6 @@ end
 
     % --- Plot 2: Normalized Stride Length ---
     figure;
-    addpath(directory + 'computation_functions/');
-    norm_SL = get_surface_matrix(filtered_joint_data, 2);
     surf(norm_SL{1}, norm_SL{2}, norm_SL{3}, norm_SL{4});
 
     xlabel("Normalized Stride Length");
@@ -180,8 +205,6 @@ end
 
     % --- Plot 3: Calculated Speed ---
     figure;
-    addpath(directory + 'computation_functions/');
-    calculated_speed = get_surface_matrix(filtered_joint_data, 3);
     surf(calculated_speed{1}, calculated_speed{2}, calculated_speed{3}, calculated_speed{4});
 
     xlabel("Calculated Speed (m/s)");
